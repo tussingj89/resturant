00010ea14d2c1faf83bc16ee10a08c1d2b611c6e
tussingj89 <tussing40@gmail.com>
2021-01-25 20:31:48 -0500

    update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var path = require("path");
+
+
 var app = express();
 var PORT = 3000;
 
@@ -13,18 +15,33 @@ app.post("/reservation", function(req, res) {
 
     var newReservation = req.body;
 
-    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+    newReservation.routeName = newReservation.name.replace();
+})
 //routes
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "resturant.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
   });
   
-  app.get("/view", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
   });
-  app.get("/reservation", function(req, res) {
-    res.sendFile(path.join(__dirname, "reservation.html"));
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+  app.get("/api/reservationArray/:reservationArray", function(req, res) {
+    var chosen = req.params.reservationsArray;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < reservationsArray.length; i++) {
+      if (chosen === reservationsArray[i].routeName) {
+        return res.json(reservationsArray[i]);
+      }
+    }
+  
+    return res.json(false);
   });
 // listener
   app.listen(PORT, function() {
